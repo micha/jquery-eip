cf6e9e56e3e386ab4c91f763455873fda9cba839
Micha Niskin <asdf@ubergibson.com>
2009-07-05 00:37:48 +0000

    2 stage

--- jquery.eip.js
+++ jquery.eip.js
@@ -134,7 +134,7 @@
 
           $(this).after(wrap);
 
-          tinyMCE.execCommand('mceAddControl', false, wrapId);
+          //tinyMCE.execCommand('mceAddControl', false, wrapId);
           return false;
 
           // ok button
