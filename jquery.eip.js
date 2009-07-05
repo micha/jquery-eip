1daa7259532258be8676ced8a853c201a51441b0
Micha Niskin <asdf@ubergibson.com>
2009-07-05 00:15:16 +0000

    2 stage

--- jquery.eip.js
+++ jquery.eip.js
@@ -127,9 +127,10 @@
           dup.css         ("top", $(this).offset().top + "px");
           dup.css         ("left", $(this).offset().left + "px");
           dup.css         ("margin", "0");
+          dup.attr("id", wrapId);
 
           $(this).after(dup);
-          dup.wrap(wrapElem);
+          //dup.wrap(wrapElem);
           tinyMCE.execCommand('mceAddControl', false, wrapId);
           return false;
 
