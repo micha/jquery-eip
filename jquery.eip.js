1a4bfa6d7aed639c84f655ea29021484de687961
Micha Niskin <asdf@ubergibson.com>
2009-07-09 16:46:03 +0000

    testing

--- jquery.eip.js
+++ jquery.eip.js
@@ -12,6 +12,17 @@
 
   var enableVal         = true;
 
+  tinyMCE.init({
+    content_css: "style.css",
+    force_root_block: false,
+    mode:  "none",
+    theme: "advanced",
+    theme_advanced_toolbar_location: "external",
+    theme_advanced_buttons1: "cut,copy,paste,|,undo,redo,|,formatselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,outdent,indent,|,link,unlink,|,blockquote,sub,sup",
+    theme_advanced_buttons2: "",
+    theme_advanced_buttons3: ""
+  });
+
   $.eip = {
     enabled : function(val) {
       return arguments.length 
